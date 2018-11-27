07906c9f4ca0794c2f847c9e85799ed2143568bc
dongxiaoli <1076454380@qq.com>
2018-11-27 15:29:28 +0800

    bug-修改

---
+++ js/url.js
@@ -0,0 +1,8 @@
+//					localStorage.setItem('myurl', 'http://27.54.248.14:8081/user')
+					localStorage.setItem('myurl01', 'http://192.168.2.245/trader')
+//					localStorage.setItem('myurl', 'http://47.104.101.165/user') //老秦
+//					localStorage.setItem('myurl', 'http://192.168.2.119/user') //祁雷
+					localStorage.setItem('myurl', 'http://192.168.2.245/user') //琼瑶姐
+//					localStorage.setItem('myurl01', 'http://47.104.101.165/user') //琼瑶姐
+//					localStorage.setItem('myimg','http://47.104.101.165')
+					localStorage.setItem('myimg','http://192.168.2.245/')
