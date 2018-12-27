ff987d60747d3a349a376cf8339cbb977f7684e7
dongxiaoli <1076454380@qq.com>
2018-12-27 11:24:32 +0800

    bug修改

--- js/url.js
+++ js/url.js
@@ -2,5 +2,5 @@
 					localStorage.setItem('myurl', 'http://47.104.101.165/user')
 					localStorage.setItem('myimg','http://47.104.101.165')
 //					47.104.101.165
-//					39.107.70.18
+//					47.104.101.165
 //					47.104.101.165
