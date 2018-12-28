7493f2a9100dbb461b833f1ea3626084fb4e140d
dongxiaoli <1076454380@qq.com>
2018-12-28 09:08:19 +0800

    bug修改

--- js/url.js
+++ js/url.js
@@ -1,6 +1,6 @@
-					localStorage.setItem('myurl01', 'http://47.104.101.165/trader')
-					localStorage.setItem('myurl', 'http://47.104.101.165/user')
-					localStorage.setItem('myimg','http://47.104.101.165')
-//					47.104.101.165
-//					47.104.101.165
+					localStorage.setItem('myurl01', 'http://39.107.70.18/trader')
+					localStorage.setItem('myurl', 'http://39.107.70.18/user')
+					localStorage.setItem('myimg','http://39.107.70.18')
+//					39.107.70.18
+//					39.107.70.18
 //					47.104.101.165
