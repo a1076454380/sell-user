d644a0d32379ab2030f59d7c6bc7de640e546e25
dongxiaoli <1076454380@qq.com>
2019-01-26 16:06:15 +0800

    bug修改

--- js/url.js
+++ js/url.js
@@ -1,6 +1,6 @@
 					localStorage.setItem('myurl01', 'http://yidianwm.com/trader')
 					localStorage.setItem('myurl', 'http://yidianwm.com/user')
 					localStorage.setItem('myimg','http://yidianwm.com')
-//					192.168.2.245
+//					yidianwm.com
 //					39.107.70.18
 //					47.104.101.165
