bb01c11ba312f7eee0e7614c0b5afb1498023312
dongxiaoli <1076454380@qq.com>
2019-07-05 15:25:43 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -1,6 +1,6 @@
-					localStorage.setItem('myurl01', 'http://192.168.2.107/trader')
-					localStorage.setItem('myurl', 'http://192.168.2.107/user')
-					localStorage.setItem('myimg','http://192.168.2.107')
+					localStorage.setItem('myurl01', 'http://192.168.2.168/trader')
+					localStorage.setItem('myurl', 'http://192.168.2.168/user')
+					localStorage.setItem('myimg','http://192.168.2.168')
 //					47.104.101.165
 //					39.107.70.18
 //					yidianwm.com
