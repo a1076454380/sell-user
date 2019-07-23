d95cf7750570b8a6e2938d91ebd33719b36fec32
dongxiaoli <1076454380@qq.com>
2019-07-23 17:46:56 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -2,5 +2,6 @@
 					localStorage.setItem('myurl', 'http://39.107.70.18/user')
 					localStorage.setItem('myimg','http://39.107.70.18')
 //					192.168.2.153
-//					47.104.101.165
+//					47.104.101.165  联华
 //					yidianwm.com
+					// 47.110.8.48	领先
