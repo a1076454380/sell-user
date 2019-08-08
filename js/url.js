9c66cabe857844e7c30a040d680c77653f91e4df
dongxiaoli <1076454380@qq.com>
2019-08-08 16:53:31 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -1,8 +1,8 @@
 					localStorage.setItem('myurl01', 'http://39.107.70.18/trader')
 					localStorage.setItem('myurl', 'http://39.107.70.18/user')
 					localStorage.setItem('myimg','http://39.107.70.18')
-//					39.107.70.18	192.168.2.153
+					// 192.168.2.168	39.107.70.18 192.168.2.168
 //					47.104.101.165  联华
 //					yidianwm.com
-//39.107.70.18
-					// 47.110.8.48	领先
+//					39.107.70.18
+// 					47.110.8.48	领先
