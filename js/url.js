3b84f52c856341af810528c855416aa797d11b3a
dongxiaoli <1076454380@qq.com>
2019-08-09 17:15:58 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -1,8 +1,8 @@
-					localStorage.setItem('myurl01', 'http://39.107.70.18/trader')
-					localStorage.setItem('myurl', 'http://39.107.70.18/user')
-					localStorage.setItem('myimg','http://39.107.70.18')
-					// 192.168.2.168	39.107.70.18 192.168.2.168
+					localStorage.setItem('myurl01', 'http://47.104.101.165/trader')
+					localStorage.setItem('myurl', 'http://47.104.101.165/user')
+					localStorage.setItem('myimg','http://47.104.101.165')
+					// 192.168.2.168	47.104.101.165 192.168.2.168
 //					47.104.101.165  联华
 //					yidianwm.com
-//					39.107.70.18
+//					47.104.101.165
 // 					47.110.8.48	领先
